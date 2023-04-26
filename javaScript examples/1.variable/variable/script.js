2ffcec4bbb9d2580748936404762e9cd269269fc
gaurav <gauravtakbhate11@gmail.com>
2023-04-27 00:00:16 +0530

    first commit

---
+++ javaScript examples/1.variable/variable/script.js
@@ -0,0 +1,23 @@
+let firstName = 'Gaurav';
+let secondName ='Takbhate'
+
+console.log(firstName,secondName,x)
+
+var x=100;
+
+console.log(x);
+
+const arr=[1,2,3,4]
+
+
+arr.push(5)
+console.log(arr);
+
+const person={
+    name:'gaurav',
+}
+
+// person.name='gt';
+person.email='gt@gmail.com'
+
+console.log(person);
---
+++ javaScript examples/1.variable/variable2/script.js
@@ -0,0 +1,31 @@
+//premitive data types
+const firstName = 'Gaurav';
+
+const number=5.5;
+
+const bool=true;
+
+const aptnumber=null
+
+let score
+
+const id=Symbol('id')
+
+const n=9090099909090009090n;
+
+
+
+//referencce data types
+
+const num=[1,2,3,4]
+
+const person={
+    name:'gaurav'
+}
+
+function sayhello(){
+    console.log('hello');
+}
+const output=sayhello;
+console.log(output,typeof output)
+
